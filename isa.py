4e2842ab1cbe41e41a8fcb1b2efda35d3a93b835
Xswinger <68491440+Xswinger@users.noreply.github.com>
2024-02-01 17:52:21 +0300

    add: src alg files, update: isa.py

--- isa.py
+++ isa.py
@@ -1,1 +1,25 @@
-class Opcode():
+import json
+from enum import Enum
+
+class Opcode(str, Enum):
+    EQUAL = "equal"
+    SUM = "sum"
+    DIF = "dif"
+
+    JMP = "jmp"
+
+
+    def __str__(self):
+        return str(self.value)
+
+def write_code(filename, code):
+    with open(filename, "w", encoding="utf-8") as file:
+        buf = []
+        for instr in code:
+            buf.append(json.dumps(instr))
+        file.write("[" + ",\n ".join(buf) + "]")
+
+
+def read_code(filename):
+    with open(filename, encoding="utf-8") as file:
+        return json.loads(file.read())  #  code
---
+++ translator.py
@@ -0,0 +1,4 @@
+def symbol_to_opcode(symbol):
+    return {
+        
+    }
