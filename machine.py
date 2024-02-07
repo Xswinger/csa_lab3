d8e84682061ecf5b74163fdc544919cb6b629c99
Xswinger <68491440+Xswinger@users.noreply.github.com>
2024-02-08 00:06:28 +0300

    Update: datapath

--- machine.py
+++ machine.py
@@ -2,7 +2,30 @@ class ALU:
     def __init__(self):
 
 class DataPath:
-    def __init__(self):
+
+    data_memory_size = None
+
+    data_memory = None
+
+    data_address = None
+
+    ac = None
+
+    alu = None
+
+    input_buffer = None
+
+    output_buffer = None
+
+    def __init__(self, data_memory_size: int, data_memory: int, input_buffer: list):
+        self.data_memory_size = data_memory_size
+        self.data_memory = [0] * data_memory_size
+        self.data_address = 0
+        self.acc = 0
+        self.alu = ALU()
+        self.input_buffer = input_buffer
+        self.output_buffer = []
+
 
 class ControlUnit:
     def __init__(self):
