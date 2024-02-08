bd5bf88d05398e24a6e3313c77bcb42490cb664a
Xswinger <68491440+Xswinger@users.noreply.github.com>
2024-02-09 00:40:02 +0300

    Update: process model in machine.py

--- machine.py
+++ machine.py
@@ -1,3 +1,5 @@
+import sys
+
 class ALU:
     def __init__(self):
 
@@ -28,8 +30,44 @@ class DataPath:
 
 
 class ControlUnit:
-    def __init__(self):
+    data_path = None
 
-def simulation():
+    _tick = None
+
+    def __init__(self, data_path: DataPath):
+        self.data_path = data_path
+        self._tick = 0
+
+    def tick(self):
+        self._tick += 1
+
+    def cur_tick(self) -> int:
+        return self._tick
+    
+    def decode_and_execute_instr(self):
+        return
+
+
+def simulation(code: list, input_tokens: list, data_memory_size: int, limit: int):
+    data_path = DataPath(data_memory_size, input_tokens)
+    control_unit = ControlUnit(code, data_path)
+    instr_counter = 0
+
+    try:
+        while instr_counter < limit:
+            control_unit.decode_and_execute_instr()
+            instr_counter += 1
+    except HaltError:
+        pass
+
+    if instr_counter >= limit:
+        return
+
 
 def main():
+    code = read_code(code_file)
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 3, "Wrong arguments: machine.py <code_file> <input_file>"
+    _, code_file, input_file = sys.argv
+    main(code_file, input_file)
